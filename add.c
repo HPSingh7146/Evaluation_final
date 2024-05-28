2dadea2ba070c57f4a6c12bd544e510aaadf2c30
Sinchanap <sinchanap89@gmail.com>
2024-05-28 07:35:21 +0530

    First commit

--- add.c
+++ add.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
+    int num3, num9, sum;
     
     printf("Enter the first number: ");
-    scanf("%d", &num1);
+    scanf("%d", &num3);
     
     printf("Enter the second number: ");
-    scanf("%d", &num2);
+    scanf("%d", &num9);
     
-    sum = num1 + num2; //for sum
+    sum = num3 + num9; //for sum
     
-    printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The sum of %d and %d is %d.", num3, num9, sum);
     
     return 0;
 }
